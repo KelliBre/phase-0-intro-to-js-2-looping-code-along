cbe1eb60e80110c67645c51f08d5662d3950ae8b
KelliBre <Kelli.Brecunier@gmail.com>
2022-12-07 11:15:50 +1000

    commit tree ec281e403e9d94624d589d4ac3be125aed2caecd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+const names=["Guadalupe", "Ollie", "Aki"];
+function writeCards( names, event ) {
+    let completedCards = []
+    for(let i = 0; i < names.length; i++){
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        console.log(completedCards);
+
+    }
+    return completedCards;
+}
+
+writeCards(names, "Suprise")
+
+//Count Down Generator
+
+let initialCount =0;
+function countDown(initialCount){
+    while (initialCount >= 0){
+        console.log(initialCount--);
+
+    }
+
+}
+
+countDown(10);
